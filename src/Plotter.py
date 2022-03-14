124b7b8a503e59454981466f704cd4a93c2b888f
angelhumberto99 <andre.gutierrez99@gmail.com>
2022-03-13 18:39:27 -0600

    añadida la pantalla de evaluación | se pueden gráficar datos | se puede reiniciar valores

--- src/Plotter.py
+++ src/Plotter.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 from matplotlib.backend_bases import MouseButton
+from matplotlib.cm import get_cmap
 from tkinter import NORMAL, DISABLED, messagebox, Tk
 from src.UI import render_main_window
 from src.MLP import MLP
+import numpy as np
 
 class Plotter:
     def __init__(self):
@@ -20,6 +22,7 @@ class Plotter:
         self.Y = [] # guarda los clusters
         self.X = [] # guarda los puntos de entrenamiento
         self.test_data = [] # guarda los puntos para evaluar
+        self.inputs = None
 
         # bandera para evaluar datos despues del entrenamiento
         self.is_training = True
@@ -38,13 +41,34 @@ class Plotter:
         self.window.mainloop()
 
     def set_point(self, event):
-        print(event.xdata, event.ydata)
+        if event.xdata != None or event.ydata != None:
+            self.X.append((event.xdata, event.ydata))
+            self.Y.append(self.input_class.get())
+            self.plot_point((event.xdata, event.ydata), self.input_class.get())
+            self.fig.canvas.draw() 
+        
+        self.input_neurons = len(np.unique(self.Y))
+        if (self.input_neurons > 1):
+            self.weight_btn["state"] = NORMAL
+
+    def mlp_can_randomize(self):
+        if self.mlp == None:
+            return False
+
+        return not self.mlp.input_neurons != self.input_neurons\
+               and self.mlp.hidden_layers != int(self.layers.get())\
+               and self.mlp.output_neurons != int(self.outputs.get())
 
     def init_weights(self):
-        if self.mlp != None:
-            self.mlp.randomize_weights()
-    def run(self):
-        pass
+        if not self.mlp_can_randomize():
+            self.mlp = MLP(self.input_neurons,
+                           int(self.layers.get()),
+                           int(self.neurons.get()), 
+                           int(self.outputs.get()))
+
+        self.mlp.randomize_weights()
+        self.mlp.print_weights()
+
     
     def plot_point(self, point: tuple, cluster=None):
         """Toma un array de tuplas y las añade los puntos en la figura con el
@@ -53,9 +77,9 @@ class Plotter:
         if (cluster == None):
             plt.plot(point[0], point[1], 'o', color='k')
         else:
-            color = 'b' if cluster == 1 else 'r'
-            shape = 'o' if cluster == 1 else 'x'
-            plt.plot(point[0], point[1], shape, color=color)
+            cmap = get_cmap('flag')
+            color = cmap(float(int(cluster)/100))
+            plt.plot(point[0], point[1], 'o', color=color)
 
     def plot_training_data(self):
         """Grafica los datos de entrenamiento"""
@@ -74,8 +98,22 @@ class Plotter:
 
     def run(self):
         """es ejecutada cuando el botón de «entrenar» es presionado"""
-        pass
+        self.params_container.grid_remove()
+        self.reset_container.grid(row=2, column=0, columnspan=6, sticky="we")
+
+
+    def evaluate(self):
+        print("evaluando")
 
     def restart(self):
         """devuelve los valores y elementos gráficos a su estado inicial"""
-        pass
+        self.clear_plot()
+        self.input_class.set(0)
+        self.learning_rate.set(0.1)
+        self.max_epoch.set(self.default_epoch)
+        self.min_error.set(0.1)
+        self.layers.set(1)
+        self.neurons.set(1)
+        self.outputs.set(1)
+        self.reset_container.grid_remove()
+        self.params_container.grid(row=2, column=0, columnspan=6, sticky="we")
--- src/UI.py
+++ src/UI.py
@@ -16,19 +16,18 @@ def render_main_window(self):
     self.window.title('Multilayer Perceptron')
 
     Label(self.window, text="MLP", bg="white", font=("Arial", 20)).grid(row=0, column=0, columnspan=6, sticky="we")
-    self.plot_container = Frame(self.window, bg="white", padx=10)
     # añade el gráfico de matplotlib a la interfaz de tkinter
-    FigureCanvasTkAgg(self.fig, self.plot_container).get_tk_widget().grid(row=0, column=0, columnspan=4)
-    Label(self.plot_container, bg="white", text="Clase: ").grid(row=1, column=1, sticky="e")
-    self.input_class = ttk.Combobox(self.plot_container)
-    self.input_class["values"] = list(range(0, 100))
-    self.input_class.set(0)
-
-    self.plot_container.grid(row=1, column=0, columnspan=6, sticky="we")
-    self.input_class.grid(row=1, column=2, sticky="w")
+    FigureCanvasTkAgg(self.fig, self.window).get_tk_widget().grid(row=1, column=0, columnspan=6, sticky="we")
 
     # contendrá un segmento de la interfaz donde se establecerán los parametros de inicio
     self.params_container = Frame(self.window, bg="white", padx=20)
+    
+    # Nos permitirá elegir la clase a dibujar en el plano
+    Label(self.params_container, bg="white", text="Clase: ").grid(row=0, column=1, columnspan=2, sticky="e")
+    self.input_class = ttk.Combobox(self.params_container)
+    self.input_class["values"] = list(range(0, 100))
+    self.input_class.set(0)
+
     # contendrá la columna izquierda de parámetros
     self.left_container = Frame(self.params_container, bg="white", padx=10, pady=20)
 
@@ -72,9 +71,9 @@ def render_main_window(self):
     self.run_btn = Button(self.right_container, text="Entrenar", command=self.run, state=DISABLED)
 
     self.params_container.grid(row=2, column=0, columnspan=6, sticky="we")
-    self.left_container.grid(row=0, column=0, columnspan=2, sticky="we")
-    self.center_container.grid(row=0, column=3, columnspan=2, sticky="we")
-    self.right_container.grid(row=0, column=5, sticky="nswe")
+    self.left_container.grid(row=1, column=0, columnspan=2, sticky="we")
+    self.center_container.grid(row=1, column=3, columnspan=2, sticky="we")
+    self.right_container.grid(row=1, column=5, sticky="nswe")
     self.learning_rate.grid(row=0, column=1, sticky="w")
     self.max_epoch.grid(row=1, column=1, sticky="w")
     self.min_error.grid(row=2, column=1, sticky="w")
@@ -83,7 +82,14 @@ def render_main_window(self):
     self.outputs.grid(row=2, column=3, sticky="w")
     self.weight_btn.grid(row=0, column=0, sticky="we")
     self.run_btn.grid(row=1, column=0, sticky="we")
+    self.input_class.grid(row=0, column=3, sticky="w")
 
+    # contenedor de la interfaz después de entrenar
+    self.reset_container = Frame(self.window, bg="white", padx=260, pady=20)
+    self.evaluate_btn = Button(self.reset_container, text="Evaluar", command=self.evaluate)
+    self.reset_btn = Button(self.reset_container, text="Reiniciar", command=self.restart)
+    self.evaluate_btn.grid(row=0, column=3, padx=15)
+    self.reset_btn.grid(row=0, column=4, padx=15)
     # escucha los eventos del mouse sobre el gráfico
     self.fig.canvas.mpl_connect('button_press_event', self.set_point)
 
